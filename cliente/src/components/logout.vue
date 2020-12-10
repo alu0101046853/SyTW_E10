ab883894bdeb6f288fe4e1c37867ac1629fd9b14
usuario <usuario@debian.iaas.ull.es>
2020-12-10 19:31:22 +0000

    De locos

---
+++ cliente/src/components/logout.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="usuario">
+    <a v-if="!user"><router-link to="/register">Registrarse</router-link></a>
+    <a v-if="!user"><router-link to="/login"> | Log in</router-link></a>
+    <button v-if="user" v-on:click="logout">Log Out</button>
+</div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "LogOut",
+  data () {
+    return {
+      user: firebase.auth().currentUser
+    };
+  },
+  methods: {
+    logout: function () {
+      firebase.auth().signOut().then(() => {
+        alert("Deslogeado Correctamente");
+        this.$router.replace("login");
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  background-color: red;
+  color: white;
+}
+</style>
