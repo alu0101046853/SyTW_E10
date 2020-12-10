2cd00c1b524ddc5a14503610f0ab366220421823
usuario <usuario@debian.iaas.ull.es>
2020-12-10 17:27:40 +0000

    De locos

--- cliente/src/views/Home.vue
+++ cliente/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <button @click="logout" id="logout" v-if="user">logut</button><br>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,11 +9,31 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import firebase from "firebase";
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      user: firebase.auth().currentUser
+    };
+  },
+  methods: {
+    logout: function () {
+      firebase.auth().signOut().then(() => {
+        alert("Deslogeado Correctamente");
+        this.$router.replace("login");
+      });
+    }
   }
 };
 </script>
+
+<style>
+#logout {
+  background-color: red;
+  color: white;
+}
+</style>
