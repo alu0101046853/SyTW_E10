4470df2ac68b47df8f4f61e7cf260e32afc13703
JoseCova <alu0101023663@ull.edu.es>
2020-12-12 18:25:06 +0000

    Creado fichero reglas eslint

---
+++ servidor/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    "spaced-comment": ["error", "always"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "dot-notation": "off",
+    "eol-last": ["error", "never"]
+  }
+}
