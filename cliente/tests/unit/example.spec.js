61db1410c3ff027721aca1f4e9bf8ca068853ee9
edfigo98 <alu0101043838@ull.edu.es>
2020-12-14 18:47:17 +0000

    Testing

--- cliente/tests/unit/example.spec.js
+++ cliente/tests/unit/example.spec.js
@@ -1,5 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
+//import { exportAllDeclaration } from "@babel/types";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Register from "@/views/Register.vue"
+import Login from "@/views/login.vue"
+//import LogOut from "@/components/logout.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -10,3 +14,40 @@ describe("HelloWorld.vue", () => {
     expect(wrapper.text()).toMatch(msg);
   });
 });
+
+describe("Register.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Register, {
+      data() {
+        return {
+          email: "hola@gmail.com",
+          password: "hola123",
+          error: false
+        };
+      }
+    });
+  });
+
+  it("user is successfully registered", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+})
+
+describe("login.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Login, {
+      data() {
+        return {
+          email: "",
+          password: ""
+        };
+      }
+    });
+  });
+
+  it("user has logged in", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+})
