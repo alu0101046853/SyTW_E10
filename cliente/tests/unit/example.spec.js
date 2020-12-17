b5a759e6ae0fa6dbf9067d6c9fed659305f89942
edfigo98 <alu0101043838@ull.edu.es>
2020-12-17 19:10:21 +0000

    Doing Testing

--- cliente/tests/unit/example.spec.js
+++ cliente/tests/unit/example.spec.js
@@ -1,17 +1,7 @@
 import { shallowMount, mount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
 import Register from "@/views/Register.vue"
 import Login from "@/views/login.vue"
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
 
 describe("Register.vue", () => {
   let wrapper;
